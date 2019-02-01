bcc877ac460b1888931615e3f8b4316485babe7a
Kellen <kellen.miller12@gmail.com>
2019-01-31 20:54:11 -0600

    Created API calls

--- API_Interface.py
+++ API_Interface.py
@@ -1,15 +1,20 @@
 import requests
 import pandas as pd
+import csv
 
-Kellen_Race_id = 1023708
 base_url = 'http://cotaaustin.clubspeedtiming.com/api/index.php/'
-key = 'key=cs-dev'
-backup_key = '&key=83468ce3017376e557b588be7fd0757d982f97e9512a04e57732f3b02f6db6a9'
-pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_columns', 20)
+
+with open('TokenKeys.csv', 'r') as token_file:
+    tokens = csv.reader(token_file, delimiter=',')
+    tokens_dict = {}
+    for token in tokens:
+        tokens_dict.update({token[0]: token[1]})
 
 def findDriversID(driverName):
-    request_url = base_url + 'racers/search.json?query=' + driverName.replace(' ', '%20') + '&' + key
+    request_url = base_url + 'racers/search.json?query=' + driverName.replace(' ', '%20') + '&' + tokens_dict['main_key']
     possible_drivers = requests.get(request_url).json()
+
     for driver in possible_drivers['racers']:
         driver.update(driver.pop('name'))
     df = pd.DataFrame(possible_drivers['racers'])
@@ -17,21 +22,62 @@ def findDriversID(driverName):
     return df
 
 def getDriversRaces(racerID):
-    request_url = base_url + 'racers/' + str(racerID) + '/races.json?&' + key
+    request_url = base_url + 'racers/' + str(racerID) + '/races.json?&' + tokens_dict['main_key']
     races_by_driver = requests.get(request_url).json()
+
     df = pd.DataFrame(races_by_driver['heats'])
     df.set_index('id', inplace=True)
     return df
 
 def getDriversInfo(racerID):
-    request_url = base_url + 'racers/' + str(racerID) + '.json?' + key
+    request_url = base_url + 'racers/' + str(racerID) + '.json?' + tokens_dict['main_key']
     driver_info = requests.get(request_url).json()
+
     driver_info['racer'].update(driver_info['racer'].pop('name'))
     df = pd.DataFrame(driver_info['racer'], index=[0])
     df.set_index('id', inplace=True)
     return df
 
+def getRaceDriverData(raceID):
+    request_url = base_url + 'races/' + str(raceID) + '.json?' + tokens_dict['main_key']
+    race_info = requests.get(request_url).json()
 
-print(findDriversID2('Kellen Miller'))
-#print(getDriversRaces(1023708))
-#print(getDriversInfo(1023708))
+    race_driver_info = []
+    for driver in race_info['race']['racers']:
+        del driver['laps']
+        race_driver_info.append(driver)
+    df = pd.DataFrame(race_driver_info)
+    df.set_index('id', inplace=True)
+    return df
+
+def getRaceLapData(raceID):
+    request_url = base_url + 'races/' + str(raceID) + '.json?' + tokens_dict['main_key']
+    race_info = requests.get(request_url).json()
+
+    driver_lap_dfs = []
+    for driver in race_info['race']['racers']:
+        df = pd.DataFrame(driver['laps'])
+        df.set_index('lap_number', inplace=True)
+        driver_lap_dfs.append(df)
+
+    return driver_lap_dfs
+
+
+def getRaceResults(raceID):
+    request_url = base_url + 'races/' + str(raceID) + '.json?' + tokens_dict['main_key']
+    race_info = requests.get(request_url).json()
+
+    df = pd.DataFrame(race_info['scoreboard'])
+    df.set_index('racer_id', inplace=True)
+    df.position = df.position.astype(int)
+    df.sort_values(by='position', inplace=True)
+    return df
+
+def getRaceData(raceID):
+    driver_data = getRaceDriverData(raceID)
+    lap_data = getRaceLapData(raceID)
+    results = getRaceResults(raceID)
+
+    return driver_data, lap_data, results
+
+print(findDriversID("Kellen Miller"))
